72fe393efad60d1e8f113cafe5e998adf744489c
TheMuellenator <themuellenator@users.noreply.github.com>
2020-01-17 18:16:46 +0000

    Removing android.support dependencies Updating gradle wrapper to 5.4.1-all.zip Updating gradle build to 3.5.3 Adding AndroidX gradle properties

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,27 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
