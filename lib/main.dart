e7f29a8d944da5cac86913fbb0452c04c09ee9de
Mukaila Semiu <semiteagain@gmail.com>
2020-05-19 11:23:18 +0100

    declear a variable and use the math library

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,26 +16,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDice() {
+       setState(() {
+                    leftDiceNumber = Random().nextInt(6)+ 1;
+                     rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+  }
+
+
+
+  @override
+ Widget build(BuildContext context) { 
     return Center(
       child: Row(
         children: <Widget> [
           Expanded(
               child: FlatButton(
                 onPressed: () {
-                  print('left button is pressed');
+                changeDice();
                 },
-                child: Image.asset('images/dice1.png')),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
             ),
 
             Expanded(
                 child: FlatButton(
                   onPressed: () {
-                    print('right button is pressed');
+                   changeDice();
                   },
-                  child: Image.asset('images/dice1.png')),
+                  child: Image.asset('images/dice$rightDiceNumber.png')),
               ),
         ],
       ),
