1e365dc29d813884749443996e581e0649b6fba3
Mukaila Semiu <semiteagain@gmail.com>
2020-05-19 08:04:06 +0100

    adding expended to image assest

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,14 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget> [
+        Expanded(
+          child: Image.asset('images/dice1.png'),),
+          Expanded(
+          
+            child: Image.asset('images/dice1.png'),),
+      ],
+    );
   }
 }
