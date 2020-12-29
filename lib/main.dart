abfaffd3fb4071ddf916bd3c5f3df7ac25714299
Abhijeet Joshi <jabhijeet@gmail.com>
2020-12-29 10:10:09 +0800

    dicee app init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueGrey.shade700,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.teal.shade300,
         ),
         body: DicePage(),
       ),
@@ -15,9 +18,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return DicePageState();
+  }
+}
+
+class DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNum.png'),
+              onPressed: () {
+                setState(() {
+                  generateRandomDiceNumber();
+                });
+                print('firstbutton: $leftDiceNum');
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                child: Image.asset('images/dice$rightDiceNum.png'),
+                onPressed: () {
+                  setState(() {
+                    generateRandomDiceNumber();
+                  });
+                  print('secondbutton: $rightDiceNum');
+                }),
+          )
+        ],
+      ),
+    );
+  }
+
+  void generateRandomDiceNumber() {
+    leftDiceNum = Random().nextInt(6) + 1;
+    rightDiceNum = Random().nextInt(6) + 1;
   }
 }
