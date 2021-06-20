1a5fedbbf5011a1b21a48757c9f9a1ae69e54720
valentine ejk <VALENTINEEJK@GMAIL.com>
2021-06-20 15:28:04 +0100

    version 1

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.lightBlue,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.lightBlueAccent,
         ),
         body: DicePage(),
       ),
@@ -15,9 +15,40 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(child: TextButton(
+            onPressed: (){
+              setState(() {
+                leftDiceNumber = 4;
+              });
+
+            },child: Image.asset('images/dice$leftDiceNumber.png'),),),
+          Expanded(child: TextButton(
+            onPressed: (){
+
+            },
+            child: Image.asset('images/dice1.png'),),),
+        ],
+      ),
+    );
   }
 }
+
+
+
+
