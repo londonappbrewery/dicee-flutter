d46f9ebe5884e4266ca7e044aef258913c2a64b4
SyLiz <44795251+SyLiz@users.noreply.github.com>
2021-08-13 12:19:30 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,8 +17,16 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -25,14 +35,31 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(14.0),
-              child: Image.asset('images/dice1.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(5) + 1;
+                    leftDiceNumber = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
             ),
           ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(14.0),
-              child: Image.asset('images/dice1.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(5) + 1;
+                    leftDiceNumber = Random().nextInt(5) + 1;
+                    //print(rightDiceNumber);
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
             ),
           ),
         ],
