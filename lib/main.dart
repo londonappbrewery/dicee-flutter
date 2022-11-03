279a673e6699f2fded3557fcf0c70ece3cb4575c
Esteban Triviño <esteban.trivino@globant.com>
2022-11-03 00:36:09 -0500

    Random dice generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  Random random = new Random();
+  int leftDiceNumber = 1;
+  int rigthDiceNumber = 4;
+
+  void geneateRandomDice() {
+    setState(() {
+      leftDiceNumber = random.nextInt(6) + 1;
+      rigthDiceNumber = random.nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+              onPressed: () {
+                geneateRandomDice();
+                print("Left dice clicked: $leftDiceNumber");
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset("images/dice$rigthDiceNumber.png"),
+              onPressed: () {
+                geneateRandomDice();
+                print("Left dice clicked: $rigthDiceNumber");
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
