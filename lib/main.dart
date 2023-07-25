64cc48d99814ef3c290e1be2ee1c2612a869c2f8
swpinchen <swpinchen@outlook.com>
2023-07-25 14:16:08 +0900

    Added static dice using Expanded Widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,17 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+            child: Image(
+              image: AssetImage('images/dice2.png'),
+            )
+        ),
+      ],
+    );
   }
 }
