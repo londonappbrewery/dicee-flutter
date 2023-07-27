82582a303809b091da98cdbbe8478c8fa58d7355
swpinchen <swpinchen@outlook.com>
2023-07-27 12:57:57 +0900

    Added stateful widget (and a few more dice)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dicee (Yahtzee Version)'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,20 +16,122 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // TO DO: Refactor as vars as an Array
+  int diceOne = 1;
+  int diceTwo = 1;
+  int diceThree = 1;
+  int diceFour = 1;
+  int diceFive = 1;
+  int diceSix = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-            child: Image(
-              image: AssetImage('images/dice2.png'),
-            )
-        ),
-      ],
+
+    void getDiceNumbers(){
+      setState(() {
+        diceOne = Random().nextInt(6) + 1;
+        diceTwo = Random().nextInt(6) + 1;
+        diceThree = Random().nextInt(6) + 1;
+        diceFour = Random().nextInt(6) + 1;
+        diceFive = Random().nextInt(6) + 1;
+        diceSix = Random().nextInt(6) + 1;
+      });
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                  ),
+                  onPressed: () {
+                    getDiceNumbers();
+                    print('Dice 1 button got pressed.');
+                  },
+                  child: Image.asset('images/dice$diceOne.png'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    getDiceNumbers();
+                    print('Dice 2 button got pressed.');
+                  },
+                  child: Image.asset('images/dice$diceTwo.png'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    getDiceNumbers();
+                    print('Dice 3 button got pressed.');
+                  },
+                  child: Image.asset('images/dice$diceThree.png'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                  ),
+                  onPressed: () {
+                    getDiceNumbers();
+                    print('Dice 4 button got pressed.');
+                  },
+                  child: Image.asset('images/dice$diceFour.png'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                  ),
+                  onPressed: () {
+                    getDiceNumbers();
+                    print('Dice 5 button got pressed.');
+                  },
+                  child: Image.asset('images/dice$diceFive.png'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    getDiceNumbers();
+                    print('Dice 6 button got pressed.');
+                  },
+                  child: Image.asset('images/dice$diceSix.png'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
+
+//
+// class DicePage extends StatelessWidget {
+//
+// }
